68f9b1efd942f5d087b5baf4780793e1c3c0f3c1
Tony Lea <tonylea@deadrobot.co.uk>
2018-10-17 21:32:04 +0100

    graphql yoga server up and running

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -1,1 +1,18 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO user express middleware to handle cookies (JWT)
+// TODO user express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => console.log(`server is not running on http://localhost:${deets.port}`)
+);
