7452956ebd1913d697e621b65416d7994380e14e
Tony Lea <tonylea@deadrobot.co.uk>
2018-10-18 20:39:54 +0100

    Add link

--- packages/frontend/pages/index.js
+++ packages/frontend/pages/index.js
@@ -1,6 +1,8 @@
+import Link from 'next/link';
+
 const Home = props => (
   <div>
-    <p>Hey</p>
+    <p>Hey!</p>
   </div>
 );
 
--- packages/frontend/pages/sell.js
+++ packages/frontend/pages/sell.js
@@ -1,6 +1,8 @@
+import Link from 'next/link';
+
 const Sell = props => (
   <div>
-    <p>Selling</p>
+    <p>Sell!</p>
   </div>
 );
 
