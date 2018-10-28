aa89d23327cac76fe759c6cf314e84b83e76e670
Tony Lea <tonylea@deadrobot.co.uk>
2018-10-28 20:58:44 +0000

    current logged in user
    using middleware and render props

--- packages/backend/src/resolvers/Query.js
+++ packages/backend/src/resolvers/Query.js
@@ -1,13 +1,21 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-  // async items(parent, args, context, info) {
-  //   const items = await context.db.query.items();
-  //   return items;
-  // },
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    // check if passed user Id
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Query;
