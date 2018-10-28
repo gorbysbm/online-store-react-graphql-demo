f97d310531effbdd6fa9c310c7c7989d263d27bb
Tony Lea <tonylea@deadrobot.co.uk>
2018-10-28 22:09:35 +0000

    sign out button

--- packages/backend/src/resolvers/Mutation.js
+++ packages/backend/src/resolvers/Mutation.js
@@ -81,6 +81,10 @@ const Mutation = {
     });
     return user;
   },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return { message: 'Goodbye!' };
+  },
 };
 
 module.exports = Mutation;
