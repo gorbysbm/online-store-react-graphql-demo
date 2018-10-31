b7219d8998101a1f5420ceb37c5f8271f76a6739
Tony Lea <tonylea@deadrobot.co.uk>
2018-10-31 19:30:53 +0000

    Add users search to backend

--- packages/backend/src/resolvers/Query.js
+++ packages/backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
   items: forwardTo('db'),
@@ -15,6 +16,14 @@ const Query = {
       },
       info
     );
+  },
+  async users(parent, args, ctx, info) {
+    // check if they are logged in
+    if (!ctx.request.userId) throw new Error(`You must be logged in`);
+    // check user has permissions to query all the users
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    // query all the users
+    return ctx.db.users({}, info);
   },
 };
 
