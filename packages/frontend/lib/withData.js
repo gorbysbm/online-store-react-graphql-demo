8103fd431337cd086eb975e9973dfef6826f63c5
Tony Lea <tonylea@deadrobot.co.uk>
2018-11-01 18:36:26 +0000

    Apollo local state and mutations

--- packages/frontend/lib/withData.js
+++ packages/frontend/lib/withData.js
@@ -1,6 +1,7 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
+import { LOCAL_STATE_QUERY } from '../components/Cart';
 
 function createClient({ headers }) {
   return new ApolloClient({
@@ -12,6 +13,25 @@ function createClient({ headers }) {
         },
         headers,
       });
+    },
+    clientState: {
+      resolvers: {
+        Mutation: {
+          toggleCart(_, args, { cache }) {
+            // read the cartOpen value from cache
+            const { cartOpen } = cache.readQuery({ query: LOCAL_STATE_QUERY });
+            // write the cart state to opposite
+            const data = {
+              data: { cartOpen: !cartOpen },
+            };
+            cache.writeData(data);
+            return data;
+          },
+        },
+      },
+      defaults: {
+        cartOpen: true,
+      },
     },
   });
 }
